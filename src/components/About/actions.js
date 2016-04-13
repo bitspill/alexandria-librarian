27f4efef9d46997bcd50093c69598ab83e130bcd
Jeremiah Buddenhagen <buddenhagenj@msoe.edu>
2016-04-12 20:57:38 -0500

    Fix About screen paths

--- src/components/About/actions.js
+++ src/components/About/actions.js
@@ -14,16 +14,16 @@ class AboutActions {
     }
 
     getLicense() {
-        request('https://raw.githubusercontent.com/dloa/alexandria-librarian/master/LICENSE.md', (error, response, body) => {
+        request('https://raw.githubusercontent.com/dloa/alexandria-librarian/master/LICENSE', (error, response, body) => {
             if (!error && response.statusCode == 200 && body)
                 this.got({
                     license: body
                 });
             else
-                fs.readFile(path.join(__dirname, '../../../../', 'LICENSE.md'), (err, data) => {
+                fs.readFile(path.join(__dirname, '../../../../', 'LICENSE'), (err, data) => {
                     if (err) return console.error(err);
                     this.got({
-                        license: data
+                        license: "" + data
                     });
                 });
         });
@@ -31,7 +31,7 @@ class AboutActions {
 
     getContributors() {
         var contributors = [];
-        request('https://raw.githubusercontent.com/dloa/alexandria-librarian/development/CONTRIBUTORS.md', (error, response, body) => {
+        request('https://raw.githubusercontent.com/dloa/alexandria-librarian/master/CONTRIBUTORS.md', (error, response, body) => {
             if (!error && response.statusCode == 200 && body) {
                 var people = body.split('# ΛLΞXΛNDRIΛ Librarian Contributors:')[1].replace('### Want to contribute?', '').replace(/\n/g, '').replace(/^\s+|\s+$/g, '').split('*').filter(Boolean);
                 people.forEach(entry => {
@@ -47,7 +47,7 @@ class AboutActions {
                     contributors: contributors
                 });
             } else {
-                fs.readFile(path.normalize(path.join(__dirname, '../../', 'CONTRIBUTORS.md')), (err, data) => {
+                fs.readFile(path.normalize(path.join(__dirname, '../../../', 'CONTRIBUTORS.md')), (err, data) => {
                     if (err) return console.log(err);
                     var people = data.toString().split('# ΛLΞXΛNDRIΛ Librarian Contributors:')[1].replace('### Want to contribute?', '').replace(/\n/g, '').replace(/^\s+|\s+$/g, '').split('*').filter(Boolean);
                     people.forEach(entry => {
